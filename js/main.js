cf8cefbdcebabe606d2cd80be78b746e2c1eefb4
{e.takahashi} <{er.takahashi4@gmail.com}>
2026-01-14 06:53:07 +0900

    1/13分 push

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+feather.replace();
+
+$('.js-heroSlider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  speed: 600,
+  arrows: true,
+  dots: true,
+  infinite: true,
+  pauseOnHover: false,
+});
+
+$(function () {
+  $('.js-heroSlider').slick({
+    dots: true,
+    arrows: true
+  });
+});
